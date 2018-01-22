d0cd1f9b23976f43a33244a8ff26eb9b4f4b3ba3
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-22 14:04:16 -0600

    update Hello

---
+++ Hello Visual World/Hello Visual World/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Hello_Visual_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickherelast = new System.Windows.Forms.Button();
+            this.btnClickHere = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickherelast
+            // 
+            this.btnClickherelast.Location = new System.Drawing.Point(96, 171);
+            this.btnClickherelast.Name = "btnClickherelast";
+            this.btnClickherelast.Size = new System.Drawing.Size(96, 23);
+            this.btnClickherelast.TabIndex = 5;
+            this.btnClickherelast.Text = "Click here last";
+            this.btnClickherelast.UseVisualStyleBackColor = true;
+            this.btnClickherelast.Click += new System.EventHandler(this.btnClickherelast_Click);
+            // 
+            // btnClickHere
+            // 
+            this.btnClickHere.Location = new System.Drawing.Point(105, 124);
+            this.btnClickHere.Name = "btnClickHere";
+            this.btnClickHere.Size = new System.Drawing.Size(75, 23);
+            this.btnClickHere.TabIndex = 4;
+            this.btnClickHere.Text = "Click here";
+            this.btnClickHere.UseVisualStyleBackColor = true;
+            this.btnClickHere.Click += new System.EventHandler(this.btnClickHere_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(93, 69);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(99, 13);
+            this.lblHelloWorld.TabIndex = 3;
+            this.lblHelloWorld.Text = "Hello, Visual World!";
+            this.lblHelloWorld.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnClickherelast);
+            this.Controls.Add(this.btnClickHere);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Name = "Form1";
+            this.Text = "Hello Visual World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickherelast;
+        private System.Windows.Forms.Button btnClickHere;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
---
+++ Hello Visual World/Hello Visual World/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hello Visual World")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Davenport Community School District")]
+[assembly: AssemblyProduct("Hello Visual World")]
+[assembly: AssemblyCopyright("Copyright © Davenport Community School District 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("caaf3934-629e-4be9-bceb-8e2b150022dc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
