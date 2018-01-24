cfb482637dacc7c7c9c04e27782484cab92e1741
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-24 13:08:35 -0600

    update GUI

---
+++ MilesToKilometersGUI/MilesToKilometersGUI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilesToKilometersGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCulcaluate_Click(object sender, EventArgs e)
+        {
+            double miles;
+            double milesToKilo = 1.6;
+            double milesToKiloConversion;
+
+            miles = Convert.ToDouble(txtMiles.Text);
+
+            milesToKiloConversion = miles * milesToKilo;
+
+            lblResults.Text = milesToKiloConversion.ToString("n");
+        }
+    }
+}
